39c1c754fb47cdf7960b402256df9b70db25684e
Nemtrack <balazsgyenes10@gmail.com>
2023-11-13 10:33:59 +0100

    Create ngStyle, ngClass example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,24 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: `.online {
+    color: white;
+  }`,
 })
 export class AppComponent {
-  title = 'WebDevTeach';
+  serverStatus = 'offline';
+  previousServerStatus = 'online';
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
+
+  onSwitchServer() {
+    if (this.serverStatus === 'offline') {
+      this.serverStatus = 'online';
+      this.previousServerStatus = 'offline';
+    } else {
+      this.serverStatus = 'offline';
+      this.previousServerStatus = 'online';
+    }
+  }
 }
